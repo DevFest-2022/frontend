6ff67813af13e61036dd530be7d7a6eb96fc536f
Sean Yu <sean.xiaochen.yu@gmail.com>
2022-03-26 16:56:16 -0400

    added search bar

--- src/core/components/SearchBar.jsx
+++ src/core/components/SearchBar.jsx
@@ -1,3 +1,30 @@
+import styled from 'styled-components';
+import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+const Form = styled.form``;
+const TextInput = styled.input``;
+const Button = styled.button``;
+
 export const SearchBar = () => {
-  return <input type='text'></input>;
+  const navigate = useNavigate();
+  const textInputRef = useRef(null);
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(textInputRef.current.value);
+    navigate('/search?q=' + textInputRef.current.value);
+  };
+
+  return (
+    <Form onSubmit={onSubmit}>
+      <TextInput
+        ref={textInputRef}
+        type='text'
+        placeholder='placeholder'
+        onSubmit={onSubmit}
+      ></TextInput>
+      <Button type='submit' onClick={onSubmit}>
+        Search
+      </Button>
+    </Form>
+  );
 };
